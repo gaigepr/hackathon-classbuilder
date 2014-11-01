311a26b16dbb76f8099e35cdfb2bbdc21823bef8
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-11-01 16:07:04 -0700

    Updated app to use http requests for data.

--- static/js/app.js
+++ static/js/app.js
@@ -1,34 +1,17 @@
 var classBrowser = angular.module('classBrowser', []);
 
-classBrowser.controller('classList', function ($scope) {
+classBrowser.controller('classList', function ($scope, $http) {
     console.log("WE are here bb");
-    $scope.classes = {
-        "CSCI 141": {
-            "credits": 4,
-            "requires": [],
-            "enables": ["CSCI 145"],
-            "fulfills": ["QSR", "CSCI MAJOR CORE"],
-            "title": "Intro to programming",
-            "description": "This is an okay class because python."
-        },
-        "CSCI 145": {
-            "credits": 4,
-            "requires": ["CSCI 141"],
-            "enables": ["CSCI 241", "CSCI 301", "CSCI 247"],
-            "fulfills": ["QSR", "CSCI MAJOR CORE"],
-            "title": "Linear data structures",
-            "description": "This is a lame class because java."
-        }
-    };
+    $scope.classes;
+
+    $http.get("/all-classes").
+        success(function(data, status, headers, config) {
+            console.log("Success: ", data, status, headers, config);
+        }).
+        error(function(data, status, headers, config) {
+            console.log("Error: ", data, status, headers, config);
+        });
+
+
 });
 
-
-
-[
-    {"className":"CSCI 141",
-     "properties":{},
-    },
-    {"className":"CSCI 145",
-     "properties":{},
-    }
-]
