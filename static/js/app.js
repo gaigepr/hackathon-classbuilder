e1558d48c55c1253c02b2727994ff7dccf0d5042
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-11-01 16:35:31 -0700

    Fixed css path and app.js

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,9 @@ classBrowser.controller('classList', function ($scope, $http) {
     $http.get("/all-classes").
         success(function(data, status, headers, config) {
             console.log("Success: ", data, status, headers, config);
+
+            $scope.classes = data;
+
         }).
         error(function(data, status, headers, config) {
             console.log("Error: ", data, status, headers, config);
